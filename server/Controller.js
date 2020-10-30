51553c47760ba6dab68abda1657918de875b652f
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-10-29 19:49:00 -0700

    first function

--- server/Controller.js
+++ server/Controller.js
@@ -5,11 +5,10 @@ module.exports = {
     
   getIngredients: (req, res) => {
     let foodArr = [];
-
-
+    foodArr.push(ingredientsList.ingredients.staples)
     res.status(200).send(foodArr)
-
   },
+
   addToList: (req, res) => {
     
   },
