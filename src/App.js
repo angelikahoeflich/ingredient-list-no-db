203531304694c24e4e850c4ae99db77531c95fe1
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-10-30 11:02:39 -0700

    added component file, added class to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component{
+  constructor(){
+    super()
+
+    this.state = {
+
+    }
+  }
+  
+  
+  render(){
   return (
-    <div className="App">
-      
+    <div>
+      <header>
+        <h1>WHAT'S IN YOUR FRIDGE?</h1>
+      </header>
+
+      <main className="main-list">
+        <section className="staples">
+          <h3>Staple Ingredients:</h3>
+        </section>
+        <section className="additionals">
+          <h3>Additional Ingredients:</h3>
+            <input type="text"/>
+        </section>
+
+        <section className="">
+          <h3>Selected Ingredients</h3>
+        </section>
+      </main>
+
+
     </div>
-  );
+    );
+  }
 }
 
 export default App;
